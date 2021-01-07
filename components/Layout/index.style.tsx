90d2d1bfbbafc9e239887e329749b1da370c7d10
ahd <ahdhani0@gmail.com>
2021-01-07 18:17:57 +0300

    increase padding of layout

--- components/Layout/index.style.tsx
+++ components/Layout/index.style.tsx
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   padding: 0 ${({ theme: { spaces } }) => spaces.sm};
   @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.md}) {
-    padding: 0 ${({ theme: { spaces } }) => spaces.xl};
+    padding: 0 6rem;
   }
 `;
