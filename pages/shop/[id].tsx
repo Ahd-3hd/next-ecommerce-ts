c460b3deb02d66fad74585d44c6e49c2916f5eca
ahd <ahdhani0@gmail.com>
2021-01-10 21:50:37 +0300

    create product details section

--- pages/shop/[id].tsx
+++ pages/shop/[id].tsx
@@ -1,35 +1,121 @@
 import Head from "next/head";
+import { useState } from "react";
+
 import {
   Wrapper,
   Carousel,
   BigImg,
   ThumbnailContainer,
   Thumbnail,
+  Container,
+  ProductDetails,
+  ProductCategoryReviewContainer,
+  CategorySpan,
+  Ratings,
+  Star,
+  ProductTitle,
+  ProductPrice,
+  CartContainer,
+  Quantity,
+  AddToCart,
+  AddToFavorite,
+  QuantityOption,
+  SizeContainer,
+  SizeLabel,
+  SizeButton,
 } from "../../styles/ProductPage.style";
 import dummyProducts from "../../dummy/dummyProducts.json";
-import { useState } from "react";
-
+import StarIcon from "../../svg/StarIcon.svg";
+import CartIcon from "../../svg/CartIcon.svg";
+import FavoriteIcon from "../../svg/FavoriteIcon.svg";
 const ProductPage = ({ product }) => {
+  const [selectedSize, setSelectedSize] = useState("sm");
   const [activeIndex, setActiveIndex] = useState(0);
+  console.log(product);
   return (
     <>
       <Head>
         <title>{product.title}</title>
       </Head>
       <Wrapper>
-        <Carousel>
-          <BigImg img={product.media[activeIndex]} />
-          <ThumbnailContainer>
-            {product.media.map((entry, index) => (
-              <Thumbnail
-                img={entry}
-                key={entry}
-                onClick={() => setActiveIndex(index)}
-                isActive={activeIndex === index}
-              />
-            ))}
-          </ThumbnailContainer>
-        </Carousel>
+        <Container>
+          <Carousel>
+            <BigImg img={product.media[activeIndex]} />
+            <ThumbnailContainer>
+              {product.media.map((entry, index) => (
+                <Thumbnail
+                  img={entry}
+                  key={entry}
+                  onClick={() => setActiveIndex(index)}
+                  isActive={activeIndex === index}
+                />
+              ))}
+            </ThumbnailContainer>
+          </Carousel>
+          <ProductDetails>
+            <ProductCategoryReviewContainer>
+              <CategorySpan>{product.category}</CategorySpan>
+              <Ratings>
+                {[...Array(5)].map((value: undefined, index: number) => (
+                  <Star key={index} active={index < product.rating}>
+                    <StarIcon />
+                  </Star>
+                ))}
+              </Ratings>
+            </ProductCategoryReviewContainer>
+            <ProductTitle>{product.title}</ProductTitle>
+            <ProductPrice>${product.price}</ProductPrice>
+            <SizeContainer>
+              <SizeLabel>Size:</SizeLabel>
+              <SizeButton
+                selected={selectedSize === "sm"}
+                onClick={() => setSelectedSize("sm")}
+              >
+                sm
+              </SizeButton>
+              <SizeButton
+                selected={selectedSize === "md"}
+                onClick={() => setSelectedSize("md")}
+              >
+                md
+              </SizeButton>
+              <SizeButton
+                selected={selectedSize === "lg"}
+                onClick={() => setSelectedSize("lg")}
+              >
+                lg
+              </SizeButton>
+              <SizeButton
+                selected={selectedSize === "xl"}
+                onClick={() => setSelectedSize("xl")}
+              >
+                xl
+              </SizeButton>
+            </SizeContainer>
+            <CartContainer>
+              <Quantity>
+                <QuantityOption value="1">1</QuantityOption>
+                <QuantityOption value="2">2</QuantityOption>
+                <QuantityOption value="3">3</QuantityOption>
+                <QuantityOption value="4">4</QuantityOption>
+                <QuantityOption value="5">5</QuantityOption>
+                <QuantityOption value="6">6</QuantityOption>
+                <QuantityOption value="7">7</QuantityOption>
+                <QuantityOption value="8">8</QuantityOption>
+                <QuantityOption value="9">9</QuantityOption>
+                <QuantityOption value="10">10</QuantityOption>
+              </Quantity>
+              <AddToCart>
+                <CartIcon />
+                Add to Cart
+              </AddToCart>
+              <AddToFavorite>
+                <FavoriteIcon />
+                Favorite
+              </AddToFavorite>
+            </CartContainer>
+          </ProductDetails>
+        </Container>
       </Wrapper>
     </>
   );
--- styles/ProductPage.style.tsx
+++ styles/ProductPage.style.tsx
@@ -2,30 +2,168 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div``;
 
+export const Container = styled.div``;
+
 export const Carousel = styled.div`
   margin: 2rem 0;
 `;
 
 export const BigImg = styled.div<{ img: string }>`
-  height: 300px;
+  height: 350px;
   background: url(${({ img }) => img}) no-repeat;
   background-size: cover;
   background-position: center;
   transition: background 0.4s ease;
+  @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.md}) {
+    height: 550px;
+  }
 `;
 
 export const ThumbnailContainer = styled.div`
   margin: 2rem 0;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 2rem;
+  display: flex;
+  background: ${({ theme: { colors } }) => colors.grey};
 `;
 export const Thumbnail = styled.div<{ img: string; isActive: boolean }>`
   height: 70px;
-  width: 100%;
+  width: 70px;
   background: url(${({ img }) => img}) no-repeat;
   background-size: cover;
   background-position: center;
   cursor: pointer;
   opacity: ${({ isActive }) => (isActive ? "1" : "0.4")};
+  margin-right: 0.5rem;
+`;
+
+export const ProductDetails = styled.div``;
+
+export const ProductCategoryReviewContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CategorySpan = styled.span`
+  font-weight: 500;
+  color: ${({ theme: { colors } }) => `${colors.primary}99`};
+  text-transform: capitalize;
+`;
+export const Ratings = styled.div`
+  display: flex;
+`;
+export const Star = styled.div<{ active: boolean }>`
+  > svg {
+    width: 15px;
+    fill: ${({ theme: { colors }, active }) =>
+      active ? colors.primary : `${colors.primary}33`};
+  }
+`;
+
+export const ProductTitle = styled.p`
+  font-weight: 600;
+  font-size: 1.5rem;
+  color: ${({ theme: { colors } }) => colors.primary};
+  margin: 0.5rem 0;
+`;
+
+export const ProductPrice = styled.p`
+  font-weight: 600;
+  font-size: 1.5rem;
+  color: ${({ theme: { colors } }) => colors.secondary};
+  margin: 0.5rem 0;
+`;
+
+export const CartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.md}) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1rem;
+  }
+`;
+
+export const Quantity = styled.select`
+  position: relative;
+  height: 3rem;
+  margin: 0.5rem 0;
+  background: #fff;
+  border: 2px solid ${({ theme: { colors } }) => colors.primary};
+  color: ${({ theme: { colors } }) => colors.primary};
+  font-weight: bold;
+  padding: 0 0.5rem;
+`;
+export const QuantityOption = styled.option``;
+export const AddToCart = styled.button`
+  height: 3rem;
+  background: ${({ theme: { colors } }) => colors.primary};
+  border: none;
+  color: #fff;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  padding: 0;
+  margin: 0.5rem 0;
+  > svg {
+    width: 20px;
+    fill: #fff;
+    margin-right: 1rem;
+  }
+`;
+export const AddToFavorite = styled.button`
+  height: 3rem;
+  background: #fff;
+  border: 2px solid ${({ theme: { colors } }) => colors.primary};
+  color: ${({ theme: { colors } }) => colors.primary};
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  padding: 0;
+  margin: 0.5rem 0;
+  > svg {
+    width: 20px;
+    fill: ${({ theme: { colors } }) => colors.primary};
+    margin-right: 1rem;
+  }
+`;
+
+export const SizeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 2rem 0;
+`;
+
+export const SizeLabel = styled.span`
+  color: ${({ theme: { colors } }) => colors.primary};
+  margin: 0;
+  font-weight: bold;
+  line-height: 2rem;
+`;
+export const SizeButton = styled.button<{ selected: boolean }>`
+  background: transparent;
+  border: none;
+  color: ${({ theme: { colors }, selected }) =>
+    selected ? colors.secondary : colors.primary};
+  padding: 0;
+  margin: 0 0.5rem;
+  font-weight: ${({ selected }) => (selected ? "bold" : "400")};
+  font-size: ${({ selected }) => (selected ? "0.85rem" : "0.7rem")};
+  line-height: 2rem;
+  transition: all 0.1s ease-in;
+  outline: none;
+  border: 1px solid
+    ${({ theme: { colors }, selected }) =>
+      selected ? colors.secondary : colors.primary};
+  width: 2.5rem;
+  height: 2.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background: ${({ theme: { colors } }) => colors.grey};
 `;
