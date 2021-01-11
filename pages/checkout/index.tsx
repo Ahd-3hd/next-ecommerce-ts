335e1213fc42c8cd8b980015a8565cfb0df0db91
ahd <ahdhani0@gmail.com>
2021-01-11 22:12:24 +0300

    create checkout page

---
+++ pages/checkout/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { Wrapper } from "../../styles/Checkout.style";
+
+const Checkout = () => {
+  return (
+    <>
+      <Head>
+        <title>Checkout</title>
+      </Head>
+      <Wrapper>Checkout page</Wrapper>
+    </>
+  );
+};
+
+export default Checkout;
