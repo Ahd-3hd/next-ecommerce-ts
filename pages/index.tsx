9e87e07650a01ae33b4e4ab3e6e2098041883a52
ahd <ahdhani0@gmail.com>
2021-01-13 08:15:15 +0300

    trying out prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,17 @@ import Brands from "../components/Brands";
 import BlogSection from "../components/BlogSection";
 import dummyProducts from "../dummy/dummyProducts.json";
 import HeaderThree from "../components/HeaderThree";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home({ products }) {
+const prisma = new PrismaClient();
+
+export default function Home({ products, users }) {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
+      <h1>{users[0].username}</h1>
       <HeaderThree />
       <ProductsShowcase products={products} title="New Arrivals" />
       <CollectionSection />
@@ -22,10 +26,13 @@ export default function Home({ products }) {
 }
 
 export async function getServerSideProps(context) {
+  const users = await prisma.user.findMany();
+  console.log(users);
   const products = dummyProducts.filter((item, i) => i < 11);
   return {
     props: {
       products,
+      users,
     },
   };
 }
